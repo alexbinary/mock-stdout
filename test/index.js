a23ea40f892887a0df4fb4b721c46c55bb6cbeae
Alexandre Bintz <alexandre@bintz.io>
2016-12-11 11:14:00 +0100

    [test] restructure tests

--- test/index.js
+++ test/index.js
@@ -6,35 +6,53 @@ let mockStdout = require('../src/index')
 describe('stdout mock', function () {
   describe('#write()', function () {
     it('takes data as input', function () {
+      // ## Setup
       let mock = mockStdout.create()
+      mock._resetData()
+      // ## TEST
       mock.write('foo')
+      // ## Assert
+      expect(mock._data).to.equal('foo')
+      // ## End
     })
   })
   describe('#_data', function () {
-    it('stores data sent to write', function () {
+    it('accumulate data sent to write', function () {
+      // ## Setup
       let mock = mockStdout.create()
       mock._resetData()
+      // ## TEST
       mock.write('foo')
       mock.write('bar')
+      // ## Assert
       expect(mock._data).to.equal('foobar')
+      // ## End
     })
   })
   describe('#_resetData()', function () {
     it('clear _data', function () {
+      // ## Setup
       let mock = mockStdout.create()
       mock._resetData()
       mock.write('foo')
+      // ## TEST
       mock._resetData()
+      // ## Assert
       expect(mock._data).to.equal('')
+      // ## End
     })
   })
   describe('#_setup()', function () {
     it('replace process.stdout with mock', function () {
+      // ## Setup
       let mock = mockStdout.create()
-      mock._setup()
       mock._resetData()
+      // ## TEST
+      mock._setup()
       process.stdout.write('foo')
+      // ## Assert
       expect(mock._data).to.equal('foo')
+      // ## End
     })
   })
 })
